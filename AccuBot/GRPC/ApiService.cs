b6a1cd18bb9e52027b845194efdabb1c2f5dba18
Stuart Johnson <stuart@factoid.org>
2022-02-26 00:27:49 +0000

    Added addional gRPC code

--- AccuBot/GRPC/ApiService.cs
+++ AccuBot/GRPC/ApiService.cs
@@ -69,7 +69,7 @@ public class ApiService : AccuBotAPI.AccuBotAPIBase
         }
         else
         {
-            var existingNode = exitingNodes.Nodes.First(x => x.NodeID == node.NodeID);
+            var existingNode = exitingNodes.Nodes.FirstOrDefault(x => x.NodeID == node.NodeID);
             if (existingNode == null)  //Incorrect ID sent!
             {
                 msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "Node not found"};
@@ -110,6 +110,25 @@ public class ApiService : AccuBotAPI.AccuBotAPIBase
         return Task.FromResult(nodelist);
     }
 
+    public override Task<MsgReply> NodeDelete(ID32 nodeID, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var existingNodes = NodeListGet(null, null).Result;  //get existing nodes.
+
+        var node = existingNodes.Nodes.FirstOrDefault(x => x.NodeID == nodeID.ID);
+        if (node==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "Node not found"};
+        }
+        else
+        {
+            existingNodes.Nodes.Remove(node);
+            File.WriteAllBytes(Path.Combine("path", "nodes"),existingNodes.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
+    }
+
     public override Task<MsgReply> UserSet(User user, ServerCallContext context)
     {
         MsgReply msgReply = null;
@@ -122,7 +141,7 @@ public class ApiService : AccuBotAPI.AccuBotAPIBase
         }
         else
         {
-            var existingUser = exitingUsers.Users.First(x => x.UserID == user.UserID);
+            var existingUser = exitingUsers.Users.FirstOrDefault(x => x.UserID == user.UserID);
             if (existingUser == null)  //Incorrect ID sent!
             {
                 msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "User not found"};
@@ -171,9 +190,54 @@ public class ApiService : AccuBotAPI.AccuBotAPIBase
         return Task.FromResult(userlist);
     }
 
-    public override Task<MsgReply> NetworkSet(Network request, ServerCallContext context)
+    public override Task<MsgReply> UserDelete(ID32 userID, ServerCallContext context)
     {
-        return base.NetworkSet(request, context);
+        MsgReply msgReply = null;
+        var existingUsers = UserListGet(null, null).Result;  //get existing nodes.
+
+        var user = existingUsers.Users.FirstOrDefault(x => x.UserID == userID.ID);
+        if (user==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "User not found"};
+        }
+        else
+        {
+            existingUsers.Users.Remove(user);
+            File.WriteAllBytes(Path.Combine("path", "userlist"),existingUsers.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
+    }
+
+    public override Task<MsgReply> NetworkSet(Network network, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var exitingNetworks = NetworkListGet(null, null).Result;  //get existing nodes.
+
+        if (network.NetworkID == 0) //id not set, so new network
+        {
+            network.NetworkID = exitingNetworks.Network.Max(x => x.NetworkID) + 1; //get new id
+            exitingNetworks.Network.Add(network);
+        }
+        else
+        {
+            var existingNetwork = exitingNetworks.Network.FirstOrDefault(x => x.NetworkID == network.NetworkID);
+            if (existingNetwork == null)  //Incorrect ID sent!
+            {
+                msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "Node not found"};
+                return Task.FromResult(msgReply);
+            }
+            else
+            {
+                existingNetwork.Name = network.Name;
+                existingNetwork.BlockTime = network.BlockTime;
+                existingNetwork.StalledAfter = network.StalledAfter;
+                existingNetwork.NotifictionID = network.NotifictionID;
+            }
+        }
+        msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok, NewID32 = network.NetworkID};
+        File.WriteAllBytes(Path.Combine("path", "networklist"),exitingNetworks.ToByteArray());
+        return Task.FromResult(msgReply);
     }
 
     public override Task<NetworkList> NetworkListGet(Empty request, ServerCallContext context)
@@ -203,38 +267,270 @@ public class ApiService : AccuBotAPI.AccuBotAPIBase
                 NotifictionID = 0,
             });
         }
-
         return Task.FromResult(networklist);
     }
 
-    public override Task<MsgReply> NodeGroupSet(NodeGroup request, ServerCallContext context)
+    public override Task<MsgReply> NetworkDelete(ID32 networkID, ServerCallContext context)
     {
-        return base.NodeGroupSet(request, context);
+        MsgReply msgReply = null;
+        var existingNetworks = NetworkListGet(null, null).Result;  //get existing nodes.
+
+        var user = existingNetworks.Network.FirstOrDefault(x => x.NetworkID == networkID.ID);
+        if (user==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "Network not found"};
+        }
+        else
+        {
+            existingNetworks.Network.Remove(user);
+            File.WriteAllBytes(Path.Combine("path", "networklist"),existingNetworks.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
+    }
+
+
+    public override Task<MsgReply> NodeGroupSet(NodeGroup group, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var exitingNodeGroup = NodeGroupListGet(null, null).Result;  //get existing nodes.
+
+        if (group.NodeGroupID == 0) //id not set, so new nodegroup
+        {
+            group.NetworkID = exitingNodeGroup.NodeGroup.Max(x => x.NodeGroupID) + 1; //get new id
+            exitingNodeGroup.NodeGroup.Add(group);
+        }
+        else
+        {
+            var existingNetwork = exitingNodeGroup.NodeGroup.FirstOrDefault(x => x.NetworkID == group.NetworkID);
+            if (existingNetwork == null)  //Incorrect ID sent!
+            {
+                msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "NodeGroup not found"};
+                return Task.FromResult(msgReply);
+            }
+            else
+            {
+                existingNetwork.Name = group.Name;
+                existingNetwork.NetworkID = group.NetworkID;
+                existingNetwork.HeightNotifictionID = group.HeightNotifictionID;
+                existingNetwork.LatencyNotifictionID = group.LatencyNotifictionID;
+                existingNetwork.PingNotifictionID = group.PingNotifictionID;
+            }
+        }
+        msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok, NewID32 = group.NetworkID};
+        File.WriteAllBytes(Path.Combine("path", "nodeGrouplist"),exitingNodeGroup.ToByteArray());
+        return Task.FromResult(msgReply);
     }
 
     public override Task<NodeGroupList> NodeGroupListGet(Empty request, ServerCallContext context)
     {
-        return base.NodeGroupListGet(request, context);
+        NodeGroupList groupList;
+        if (File.Exists(Path.Combine("path","networklist")))
+        {  //Read from file
+            groupList = NodeGroupList.Parser.ParseFrom(File.ReadAllBytes(Path.Combine("path", "nodeGrouplist")));
+        }
+        else
+        {
+            groupList = new NodeGroupList();
+            groupList.NodeGroup.Add(new NodeGroup()
+            {
+                NodeGroupID = 1,
+                Name = "Validator",
+                NetworkID = 1,
+                PingNotifictionID = 1,
+                HeightNotifictionID = 1,
+                LatencyNotifictionID = 1
+            });
+            groupList.NodeGroup.Add(new NodeGroup
+            {
+                NodeGroupID = 2,
+                Name = "Follower",
+                NetworkID = 1,
+                PingNotifictionID = 1,
+                HeightNotifictionID = 1,
+                LatencyNotifictionID = 1
+            });
+        }
+        return Task.FromResult(groupList);
+    }
+
+    public override Task<MsgReply> NodeGroupDelete(ID32 nodeGroupID, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var existingNodeGroup = NodeGroupListGet(null, null).Result;  //get existing nodes.
+
+        var nodegroup = existingNodeGroup.NodeGroup.FirstOrDefault(x => x.NetworkID == nodeGroupID.ID);
+        if (nodegroup==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "NodeGroup not found"};
+        }
+        else
+        {
+            existingNodeGroup.NodeGroup.Remove(nodegroup);
+            File.WriteAllBytes(Path.Combine("path", "nodeGrouplist"),existingNodeGroup.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
     }
 
     public override Task<DomainCertificateList> DomainCertificateListGet(Empty request, ServerCallContext context)
     {
-        return base.DomainCertificateListGet(request, context);
+        DomainCertificateList domainCertificateList;
+        if (File.Exists(Path.Combine("path","domainCertificateList")))
+        {  //Read from file
+            domainCertificateList = DomainCertificateList.Parser.ParseFrom(File.ReadAllBytes(Path.Combine("path", "domainCertificateList")));
+        }
+        else
+        {
+            domainCertificateList = new DomainCertificateList();
+            domainCertificateList.DomainCertificate.Add(new DomainCertificate
+            {
+                DomainCertificateID = 1,
+                Domain = "mydomain.com",
+                Monitor = false
+            });
+        }
+        return Task.FromResult(domainCertificateList);
     }
 
-    public override Task<MsgReply> DomainCertificateSet(DomainCertificate request, ServerCallContext context)
+    public override Task<MsgReply> DomainCertificateSet(DomainCertificate domainCertificate, ServerCallContext context)
     {
-        return base.DomainCertificateSet(request, context);
+        MsgReply msgReply = null;
+        var exitingDomainCertificate = DomainCertificateListGet(null, null).Result;  //get existing nodes.
+
+        if (domainCertificate.DomainCertificateID == 0) //id not set, so new cert
+        {
+            domainCertificate.DomainCertificateID = exitingDomainCertificate.DomainCertificate.Max(x => x.DomainCertificateID) + 1; //get new id
+            exitingDomainCertificate.DomainCertificate.Add(domainCertificate);
+        }
+        else
+        {
+            var existingDomainCertificate = exitingDomainCertificate.DomainCertificate.FirstOrDefault(x => x.DomainCertificateID == domainCertificate.DomainCertificateID);
+            if (existingDomainCertificate == null)  //Incorrect ID sent!
+            {
+                msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "DomainCertificate not found"};
+                return Task.FromResult(msgReply);
+            }
+            else
+            {
+                existingDomainCertificate.Domain = domainCertificate.Domain;
+                existingDomainCertificate.Monitor = domainCertificate.Monitor;
+                existingDomainCertificate.DomainCertificateID = domainCertificate.DomainCertificateID;
+            }
+        }
+        msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok, NewID32 = domainCertificate.DomainCertificateID};
+        File.WriteAllBytes(Path.Combine("path", "domainCertificateList"),exitingDomainCertificate.ToByteArray());
+        return Task.FromResult(msgReply);
+    }
+
+    public override Task<MsgReply> DomainCertificateDelete(ID32 domainCertificateID, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var existingDomainCertificate = DomainCertificateListGet(null, null).Result;  //get existing nodes.
+
+        var domainCertificate = existingDomainCertificate.DomainCertificate.FirstOrDefault(x => x.DomainCertificateID == domainCertificateID.ID);
+        if (domainCertificate==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "DomainCertificate not found"};
+        }
+        else
+        {
+            existingDomainCertificate.DomainCertificate.Remove(domainCertificate);
+            File.WriteAllBytes(Path.Combine("path", "domainCertificateList"),existingDomainCertificate.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
     }
 
     public override Task<NotificationPolicyList> NotificationPolicyListGet(Empty request, ServerCallContext context)
     {
-        return base.NotificationPolicyListGet(request, context);
+        NotificationPolicyList notificationPolicyList;
+        if (File.Exists(Path.Combine("path","notificationPolicyList")))
+        {  //Read from file
+            notificationPolicyList = NotificationPolicyList.Parser.ParseFrom(File.ReadAllBytes(Path.Combine("path", "notificationPolicyList")));
+        }
+        else
+        {
+            notificationPolicyList = new NotificationPolicyList();
+            notificationPolicyList.NotificationPolicyList_.Add(new NotificationPolicy
+            {
+                NotifictionID = 1,
+                Name = "Nothing",
+                Discord = -1,
+                Call = -1
+            });
+            notificationPolicyList.NotificationPolicyList_.Add(new NotificationPolicy
+            {
+                NotifictionID = 2,
+                Name = "Warning",
+                Discord = 120,
+                Call = -1
+            });
+            notificationPolicyList.NotificationPolicyList_.Add(new NotificationPolicy
+            {
+                NotifictionID = 3,
+                Name = "Alert",
+                Discord = 0,
+                Call = 600
+            });
+            notificationPolicyList.NotificationPolicyList_.Add(new NotificationPolicy
+            {
+                NotifictionID = 4,
+                Name = "Panic",
+                Discord = 0,
+                Call = 0
+            });
+            
+        }
+        return Task.FromResult(notificationPolicyList);
     }
 
-    public override Task<MsgReply> NotificationPolicySet(NotificationPolicy request, ServerCallContext context)
+    public override Task<MsgReply> NotificationPolicySet(NotificationPolicy notificationPolicy, ServerCallContext context)
     {
-        return base.NotificationPolicySet(request, context);
+        MsgReply msgReply = null;
+        var exitingNotifiactionPolicy = NotificationPolicyListGet(null, null).Result;  //get existing nodes.
+
+        if (notificationPolicy.NotifictionID == 0) //id not set, so new policy
+        {
+            notificationPolicy.NotifictionID = exitingNotifiactionPolicy.NotificationPolicyList_.Max(x => x.NotifictionID) + 1; //get new id
+            exitingNotifiactionPolicy.NotificationPolicyList_.Add(notificationPolicy);
+        }
+        else
+        {
+            var existingNotificationPolicy = exitingNotifiactionPolicy.NotificationPolicyList_.FirstOrDefault(x => x.NotifictionID == notificationPolicy.NotifictionID);
+            if (existingNotificationPolicy == null)  //Incorrect ID sent!
+            {
+                msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "NotificationPolicy not found"};
+                return Task.FromResult(msgReply);
+            }
+            else
+            {
+                existingNotificationPolicy.Name = notificationPolicy.Name;
+                existingNotificationPolicy.Call = notificationPolicy.Call;
+                existingNotificationPolicy.Discord = notificationPolicy.Discord;
+            }
+        }
+        msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok, NewID32 = notificationPolicy.NotifictionID};
+        File.WriteAllBytes(Path.Combine("path", "notificationPolicyList"),exitingNotifiactionPolicy.ToByteArray());
+        return Task.FromResult(msgReply);
     }
-    
+
+    public override Task<MsgReply> NotificationPolicyDelete(ID32 notificationPolicyID, ServerCallContext context)
+    {
+        MsgReply msgReply = null;
+        var existingNotificationPolicy = NotificationPolicyListGet(null, null).Result;  //get existing nodes.
+
+        var notificationPolicy = existingNotificationPolicy.NotificationPolicyList_.FirstOrDefault(x => x.NotifictionID == notificationPolicyID.ID);
+        if (notificationPolicy==null)
+        {
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Fail, Message = "NotificationPolicy not found"};
+        }
+        else
+        {
+            existingNotificationPolicy.NotificationPolicyList_.Remove(notificationPolicy);
+            File.WriteAllBytes(Path.Combine("path", "notificationPolicyList"),existingNotificationPolicy.ToByteArray());
+            msgReply = new MsgReply() { Status = MsgReply.Types.Status.Ok};
+        }
+        return Task.FromResult(msgReply);
+    }
 }
