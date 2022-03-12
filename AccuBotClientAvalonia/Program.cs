4422ad121e0cf7f6bcbce7133dbf27a97928c770
Stuart Johnson <stuart@factoid.org>
2022-03-12 20:46:31 +0000

    Avalonia Client demo

---
+++ AccuBotClientAvalonia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AccuBotCommon;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
+
+namespace AccuBotClientAvalonia
+{
+    public static class Program
+    {
+
+        public static AccuBotClient gRPCClient = null;
+        
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            gRPCClient = new AccuBotClient("https://localhost:5001");
+            gRPCClient.Connect();
+            
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .UseReactiveUI();
+    }
+}
---
+++ AccuBotClientAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AccuBotClientAvalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public string Greeting
+        {
+            get
+            {
+                try
+                {
+                    var user = AccuBotClientAvalonia.Program.gRPCClient.API.UserListGet(new Empty());
+                    return $"Welcome to Avalonia \r\n\r\n Name:{user.Users[0].Name}";
+                }
+                catch (Exception ex)
+                {
+                    return $"Welcome to Avalonia! \r\n\r\n gRPC Error:{ex.Message}";
+                }
+            }
+        }
+    }
+}
